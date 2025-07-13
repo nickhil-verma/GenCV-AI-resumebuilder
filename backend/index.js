07e21bd33a8beb73344171ff114d3cda1384ec1c
Nick <137506342+nickhil-verma@users.noreply.github.com>
2025-07-13 23:32:55 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,16 +9,21 @@ require('dotenv').config();
 require('./Models/db');
 const PORT = process.env.PORT || 8080;
 
+// Allow CORS from specific origin
+app.use(cors({
+    origin: 'https://gen-cv-ai-resumebuilder-frontend.vercel.app',
+    credentials: true // if you're using cookies, sessions, or auth headers
+}));
+
+app.use(bodyParser.json());
+
 app.get('/hello', (req, res) => {
     res.send('world!');
 });
 
-app.use(bodyParser.json());
-app.use(cors());
 app.use('/auth', AuthRouter);
 app.use('/products', ProductRouter);
 
-
 app.listen(PORT, () => {
-    console.log(`Server is running on ${PORT}`)
-})
+    console.log(`Server is running on ${PORT}`);
+});
